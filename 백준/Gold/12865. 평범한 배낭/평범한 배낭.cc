ddbdd70703d37cfc48bef1024988f71e81684527
yukiys <150210372+yukiys@users.noreply.github.com>
2025-01-28 16:55:47 +0900

    [Gold V] Title: 평범한 배낭, Time: 40 ms, Memory: 41476 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int N,K;
+int W[101];
+int V[101];
+int dp[101][100001];
+
+int main()
+{   
+    cin>>N>>K;
+    for(int i=1;i<=N;i++)
+        cin>>W[i]>>V[i];
+
+    for(int i=1;i<=N;i++)
+    {
+        for(int j=1;j<=K;j++)
+        {
+            if(j>=W[i]) dp[i][j]=max(dp[i-1][j],dp[i-1][j-W[i]]+V[i]);
+            else dp[i][j]=dp[i-1][j];
+        }
+    }
+
+    cout<<dp[N][K]<<"\n";
+
+    return 0;
+}
