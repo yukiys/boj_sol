8acde197f8bd33f3d5a38d56a3983eb2d2cd4604
yukiys <150210372+yukiys@users.noreply.github.com>
2025-07-10 20:06:27 +0900

    [Gold IV] Title: 두 부분 문자열, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/11056. 두 부분 문자열/두 부분 문자열.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+string A,B;
+int LCS[1001][1001];
+
+int main()
+{
+    cin>>A>>B;
+    A="0"+A;
+    B="0"+B;
+
+    for(int i=0;i<A.size();i++)
+    {
+        for(int j=0;j<B.size();j++)
+        {
+            if(i==0 or j==0) LCS[i][j]=0;
+            else if(A[i]==B[j]) LCS[i][j]=LCS[i-1][j-1]+1;
+            else LCS[i][j]=max(LCS[i-1][j],LCS[i][j-1]);
+        }
+    }
+
+    cout<<A.size()+B.size()-2-LCS[A.size()-1][B.size()-1]<<"\n";
+
+    return 0;
+}
