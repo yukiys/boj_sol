ce0284f9aa0eeb68ec682bfff82967da863ab549
yukiys <150210372+yukiys@users.noreply.github.com>
2025-08-30 13:02:20 +0900

    [Gold III] Title: 별자리 만들기, Time: 0 ms, Memory: 2324 KB -BaekjoonHub

---
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int N,cnt;
+double ret;
+pair<double,double> stars[101];
+int parent[4951];
+vector<pair<double,pair<int,int>>> v;
+
+int Find(int a)
+{
+    if(parent[a]==a) return a;
+    return parent[a]=Find(parent[a]);
+}
+
+void Union(int a,int b)
+{
+    a=Find(a);
+    b=Find(b);
+    if(a==b) return;
+
+    if(a<b) parent[b]=a;
+    else parent[a]=b;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin>>N;
+
+    for(int i=1;i<=N;i++) cin>>stars[i].first>>stars[i].second;
+
+    for(int i=1;i<=N;i++)
+    {
+        parent[i]=i;
+        
+        for(int j=i+1;j<=N;j++)
+        {
+            double dist=(stars[i].first-stars[j].first)*(stars[i].first-stars[j].first);
+            dist+=(stars[i].second-stars[j].second)*(stars[i].second-stars[j].second);
+            dist=sqrt(dist);
+
+            v.push_back({dist,{i,j}});
+        }
+    }
+    
+    sort(v.begin(),v.end());
+    for(int i=0;i<v.size();i++)
+    {
+        double dist=v[i].first;
+        int now=v[i].second.first;
+        int next=v[i].second.second;
+
+        if(Find(now)==Find(next)) continue;
+
+        cnt++;
+        Union(now,next);
+        ret+=dist;
+
+        if(cnt==N-1) break;
+    }
+
+    cout<<fixed;
+    cout.precision(2);
+    cout<<ret<<"\n";
+
+    return 0;
+}
