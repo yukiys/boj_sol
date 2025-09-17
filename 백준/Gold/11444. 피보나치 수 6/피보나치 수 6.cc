cfdeb78d35e75efd3383714bf4d211b067413ad1
yukiys <150210372+yukiys@users.noreply.github.com>
2025-09-17 09:07:34 +0900

    [Gold II] Title: 피보나치 수 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+
+#define MOD 1000000007
+
+using namespace std;
+
+long long N;
+long long matrix[2][2]={{1,1},{1,0}};
+long long ret[2][2]={{1,0},{0,1}};
+
+void input()
+{
+    cin>>N;
+}
+
+void MatrixMul(long long a[2][2],long long b[2][2])
+{
+    long long c[2][2];
+
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            c[i][j]=0;
+            for(int k=0;k<2;k++) c[i][j]+=a[i][k]*b[k][j];
+            c[i][j]%=MOD;
+        }
+    }
+
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            a[i][j]=c[i][j];
+}
+
+void solve()
+{
+    while(N>0)
+    {
+        if(N%2==1) MatrixMul(ret,matrix);
+        MatrixMul(matrix,matrix);
+        N/=2;
+    }
+}
+
+void printResult()
+{
+    cout<<ret[0][1]<<"\n";
+}
+
+int main()
+{
+    input();
+    solve();
+    printResult();
+
+    return 0;
+}
