3a5b34d57ea8f0afc49a64fb58a1a455204f4159
yukiys <150210372+yukiys@users.noreply.github.com>
2026-03-19 16:56:33 +0900

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+    cin >> N;
+
+    if(N % 2 == 0)
+        cout << "SK" << "\n";
+    else
+        cout << "CY" << "\n";
+}
